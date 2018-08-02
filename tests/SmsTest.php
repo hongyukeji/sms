3a379893d87c25dc55d6116c9a29705eeb4c1a25
Shadow <admin@hongyuvip.com>
2018-08-02 23:50:31 +0800

    Initial commit

--- tests/SmsTest.php
+++ tests/SmsTest.php
@@ -23,35 +23,21 @@ class SmsTest
         $config = [
             'yunpian' => [
                 'apikey' => '',
-                'templateCode' => [
-                    // 您的验证码是#code#。有效期为#hour#，请尽快验证！
-                    'verificationCode' => '2414994',
-                ],
             ],
             'aliyun' => [
                 'accessKeyId' => '',
                 'accessKeySecret' => '',
                 'signName' => '',
-                'templateCode' => [
-                    // 您的验证码是${code}。有效期为${hour}，请尽快验证！
-                    'verificationCode' => '',
-                ],
             ],
             'qcloud' => [
                 'appid' => '',
                 'appkey' => '',
                 'smsSign' => '',
-                'templateCode' => [
-                    'verificationCode' => '',
-                ],
             ],
             'duanxinbao' => [
                 'user' => '',
                 'pass' => '',
                 'signName' => '',
-                'templateCode' => [
-                    'verificationCode' => '您的验证码是%s。有效期为%s，请尽快验证！',
-                ],
             ],
         ];
 
