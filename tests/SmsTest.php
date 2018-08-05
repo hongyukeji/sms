14684c4702e96837b42a8b16330f7c8c478c3920
Shadow <admin@hongyuvip.com>
2018-08-05 12:59:47 +0800

    Initial commit

--- tests/SmsTest.php
+++ tests/SmsTest.php
@@ -4,6 +4,8 @@ namespace HongYuKeJi\Helpers\Tests;
 
 require_once dirname(__FILE__) . '/../example/config.php';
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use HongYuKeJi\Helpers\Sms;
 use HongYuKeJi\Helpers\Tests\TestCase;
 
@@ -57,7 +59,8 @@ class SmsTest extends TestCase
             'time' => '15分钟',
         ]);
 
-        var_dump($resultTemplateCode);
+        //var_dump($resultTemplateCode);
+        $this->log()->info('resultTemplateCode', $resultTemplateCode);
 
         // Content + templateParam
         $resultContent = $sms->send('13800138000', '您的验证码是%s。有效期为%s，请尽快验证！', [
@@ -65,14 +68,16 @@ class SmsTest extends TestCase
             'time' => '15分钟',
         ], 'duanxinbao');
 
-        var_dump($resultContent);
+        //var_dump($resultContent);
+        $this->log()->info('resultContent', $resultContent);
 
         $result = $sms->send('13800138000', '您的验证码是%s。有效期为%s，请尽快验证！', [
             'code' => '1234',
             'time' => '15分钟',
         ], 'duanxinbao');
 
-        var_dump($result);
+        //var_dump($result);
+        $this->log()->info('result', $result);
 
         $this->assertEquals(0, 0);
         //$this->assertEquals(0, $result['statusCode']);
@@ -83,10 +88,26 @@ class SmsTest extends TestCase
         $stack = [];
         $this->assertEquals(0, count($stack));
         array_push($stack, 'foo');
+
+        //$this->log()->info('info', $stack);
+
         $this->assertEquals('foo', $stack[count($stack) - 1]);
         $this->assertEquals(1, count($stack));
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
 
+    public function log()
+    {
+        $logger = new Logger('sms_logger');
+        // Now add some handlers
+        try {
+            $logger->pushHandler(new StreamHandler(__DIR__ . '/../storage/logs/app.log', Logger::DEBUG));
+        } catch (\Exception $e) {
+            print_r($e);
+        }
+        // You can now use your logger
+        //$logger->info('My logger is now ready');
+        return $logger;
+    }
 }
